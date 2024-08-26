be9e7f102969e51be093c108c1b4a17c7941550d
glezma <gonzalolezma@gmail.com>
2024-08-25 21:03:06 -0600

    test de correccion de codigo

--- test.py
+++ test.py
@@ -1,14 +1,14 @@
 # %%
-
 import pandas as pd
+import utils as ut
 df = pd.DataFrame()
 d = {'columna1': [1, 2], 'columna2': [3, 4]}
 df = pd.DataFrame(data=d)
-df
-print(df)
-# %%
-import utils as ut
+# utilizando mi function suma
 misuma = ut.suma(1,2)
-# %%
 df1 = ut.procesarpandas(df)
+
+# descargar df en archivo csv sin indice
+df1.to_csv('archivo.csv', index=False)
+
 # %%
